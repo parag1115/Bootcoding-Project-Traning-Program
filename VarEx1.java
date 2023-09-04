68667055fe422c66dd3ca33f29410442de396c74
parag1115 <144015206+parag1115@users.noreply.github.com>
2023-09-04 16:54:13 +0530

    Create VarEx1.java
    one file added

---
+++ VarEx1.java
@@ -0,0 +1,14 @@
+class VarEx1 {
+    public static void main(String[] args) {
+        // create variable
+        //data type var-name;
+        int rollNo;
+        rollNo= 10;
+        int age = 24;
+        double price = 105.55;
+        boolean isFree = true;
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(isFree);
+    }
+}
